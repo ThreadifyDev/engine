1125167264904ba47b2f1546fef29de4de5bdcb6
CreativeJoe007 <team9.tech@gmail.com>
2026-04-21 01:42:49 +0100

    ..

--- web/app/root.tsx
+++ web/app/root.tsx
@@ -14,6 +14,9 @@ import { getConfig } from "./config.server";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: stylesheet },
+  { rel: "icon", type: "image/svg+xml", href: "/favicon-black.svg", media: "(prefers-color-scheme: light)" },
+  { rel: "icon", type: "image/svg+xml", href: "/favicon-white.svg", media: "(prefers-color-scheme: dark)" },
+  { rel: "icon", type: "image/svg+xml", href: "/favicon-black.svg" },
 ];
 
 export const loader = async () => {
