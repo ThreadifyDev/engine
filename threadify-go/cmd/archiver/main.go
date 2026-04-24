6a91c3af918911bd4d503e63b4cef3d03f0561fa
CreativeJoe007 <team9.tech@gmail.com>
2026-04-24 01:18:48 +0100

    feat: archiver service updates

--- threadify-go/cmd/archiver/main.go
+++ threadify-go/cmd/archiver/main.go
@@ -44,14 +44,15 @@ func main() {
 }
 
 func run(configPath string, logger *zap.Logger) error {
-	viper.SetConfigFile(configPath)
-	viper.AutomaticEnv()
-	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
-	if err := viper.ReadInConfig(); err != nil {
+	v := viper.New()
+	v.SetConfigFile(configPath)
+	v.AutomaticEnv()
+	v.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	if err := v.ReadInConfig(); err != nil {
 		return fmt.Errorf("read config: %w", err)
 	}
 
-	cfg, err := appconfig.LoadFromViper()
+	cfg, err := appconfig.LoadFromViper(v)
 	if err != nil {
 		return fmt.Errorf("load config: %w", err)
 	}
